dcf790b332870831d7a535b28ece8d6f6d7a5425
Colin Tindle <colin.tindle@financial-cloud.com>
2018-10-22 17:31:27 +0100

    add: window and postcss styles working

--- app/src/components/window/Window.jsx
+++ app/src/components/window/Window.jsx
@@ -1,11 +1,43 @@
 import React, { Fragment } from 'react'
-import styles from './window.scss'
+import * as window from './window.scss'
 
 const Window = ({ someText }) => {
+  const style = {
+    left: '10px',
+    top: '10px',
+    width: '300px',
+    height: '200px'
+  }
+
+  console.log({ window })
+
   return (
-    <Fragment>
-      <p className={styles.window}>{`My ${someText} Window`}</p>
-    </Fragment>
+    <div className={window.window} style={style}>
+      <button className={window.window__button}>
+        <i className="fas fa-times" />
+      </button>
+      <div className={window.window__title} />
+      <button className={window.window__button}>
+        <i className="fas fa-window-minimize" />
+      </button>
+      <button className={window.window__button}>
+        <i className="fas fa-expand-arrows-alt" />
+      </button>
+      <div className={window.window__content}>
+        <iframe
+          className={window.window__content__iframe}
+          src="about:blank"
+          scrolling="yes"
+        />
+        <div className={window.window__content__loading}>Loading...</div>
+        <div className={window.window__content__cover} />
+      </div>
+      <button className={`${window.window__button} ${window.size}`}>
+        <svg viewBox="0 0 10 10">
+          <line x1="0" y1="100%" x2="100%" y2="0" stroke="black" />
+        </svg>
+      </button>
+    </div>
   )
 }
 
